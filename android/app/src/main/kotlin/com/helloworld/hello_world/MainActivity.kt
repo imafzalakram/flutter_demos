0af08631a78af905b5d69450ed51022c1c3ea6fe
Muhammad Afzal <imafzalakram@gmail.com>
2022-05-22 11:30:27 +0500

    Hello World app using Text and TextStyle widget

---
+++ android/app/src/main/kotlin/com/helloworld/hello_world/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.helloworld.hello_world
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Text(
+      "Hello World!",
+      textDirection: TextDirection.ltr,
+      style: TextStyle(
+        color: Colors.lightBlue,
+        fontSize: 40.0,
+      ),
+    ),
+  );
+}
+
+// Ctl+Shift+alt+L to reformat the code on Windows
+// Cmd+Shift+Opt+L to reformate the code on Mac
