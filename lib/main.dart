b940d1b3b17776e3fe5d360f45e6a99686d3ef6c
Muhammad Afzal <imafzalakram@gmail.com>
2022-05-22 12:15:20 +0500

    Container widget and its properties

--- lib/main.dart
+++ lib/main.dart
@@ -3,22 +3,27 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "App Bar",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "App Bar",
+          ),
         ),
-      ),
-      body: Center(
-        child: Text(
-          "Hello World!",
-          textDirection: TextDirection.ltr,
-          style: TextStyle(
-            color: Colors.lightBlue,
-            fontSize: 40.0,
+        body: Container(
+          color: Colors.lightBlueAccent,
+          margin: EdgeInsets.all(30.0),    // it will give space to Container widget from outside
+          padding: EdgeInsets.all(25.0),   // it will give space to Container widget from inside which will push Text to move accordingly
+          child: Text(
+            "Hello World!",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+              backgroundColor: Colors.black,
+              color: Colors.white,
+              fontSize: 30.0,
+            ),
           ),
         ),
       ),
-    )),
+    ),
   );
 }
