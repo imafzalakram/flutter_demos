6c83c2067fa48db7853f577cdf5009692b926f6c
Muhammad Afzal <imafzalakram@gmail.com>
2022-05-22 11:44:45 +0500

    Material App widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
-
 void main() {
   runApp(
-    Center(
-      child:  Text(
-        "Hello World!",
-        textDirection: TextDirection.ltr,
-        style: TextStyle(
-          color: Colors.lightBlue,
-          fontSize: 40.0,
+    MaterialApp(
+      home: Center(
+        child: Text(
+          "Hello World!",
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            color: Colors.lightBlue,
+            fontSize: 40.0,
+          ),
         ),
       ),
-    )
+    ),
   );
 }
