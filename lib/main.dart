922d5832329fe054501c75ddfd81acd5ba21d54e
Muhammad Afzal <imafzalakram@gmail.com>
2022-05-22 17:41:35 +0500

    Nested Column & Rows Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -4,21 +4,119 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "App Bar",
+          appBar: AppBar(
+            title: Text(
+              "App Bar",
+            ),
           ),
-        ),
-        body: Container(
-          child: Column(
-            children: [
-              Text("First Child"),
-              Text("Second Child"),
-              Text("Third Child")
-            ],
-          ),
-        )
-      ),
+          body: Container(
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child:
+                      Container(
+                        color: Colors.green,
+                        child: Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Text("April"),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child:
+                      Container(
+                        color: Colors.yellow,
+                        child: Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Text("May"),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child:
+                      Container(
+                        color: Colors.lightGreenAccent,
+                        child: Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Text("June"),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child:
+                      Container(
+                        color: Colors.pink,
+                        child: Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Text("July"),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child:
+                      Container(
+                        color: Colors.amber,
+                        child: Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Text("Aug"),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child:
+                      Container(
+                        color: Colors.lightGreen,
+                        child: Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Text("Sep"),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child:
+                      Container(
+                        color: Colors.greenAccent,
+                        child: Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Text("Oct"),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child:
+                      Container(
+                        color: Colors.blue,
+                        child: Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Text("Nov"),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child:
+                      Container(
+                        color: Colors.brown,
+                        child: Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Text("Dec"),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          )),
     ),
   );
 }
