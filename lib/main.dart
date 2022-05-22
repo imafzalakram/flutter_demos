ae4ce0d2c1507370d71cb63ae0dc7c69795a4770
Muhammad Afzal <77921765+imafzalakram@users.noreply.github.com>
2022-05-22 12:52:46 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,25 @@
-import 'package:flutter/material.dart';
-
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text(
-              "App Bar",
-            ),
+        appBar: AppBar(
+          title: Text(
+            "App Bar",
           ),
-          body: Material(
-            child: Container(
-              alignment: Alignment.centerRight,
-              child: Text(
-                "Hello World!",
-                style: TextStyle(
-                  color: Colors.lightBlueAccent,
-                  fontSize: 30.0,
-                ),
+        ),
+        body: Material(
+          child: Container(
+            alignment: Alignment.centerRight,
+            child: Text(
+              "Hello World!",
+              style: TextStyle(
+                color: Colors.lightBlueAccent,
+                fontSize: 30.0,
               ),
             ),
-          )),
+          ),
+        ),
+      ),
     ),
   );
 }
