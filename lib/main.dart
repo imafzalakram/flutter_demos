424a1058461514d07c4ef874b6e27dcaaab03219
Muhammad Afzal <imafzalakram@gmail.com>
2022-05-29 10:57:28 +0500

    Source code of Banking Calculator

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,149 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(StudentItro());
+  runApp(MaterialApp(
+    title: 'Simple Interest Calculator App',
+    home: SIForm(),
+  ));
 }
 
-class StudentItro extends StatefulWidget {
+class SIForm extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return StudentIntroStates();
+    return _SIFormState();
   }
 }
 
-class StudentIntroStates extends State<StudentItro> {
-  String studentIntro="Empty";
+class _SIFormState extends State<SIForm> {
+  var _currencies = ['Rupees', 'Dollars', 'Pounds'];
+  final double _minimumPadding = 5.0;
+  String dropdownValue = 'Rupee';
 
   @override
   Widget build(BuildContext context) {
-    debugPrint("calling build function");
-
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text("Handeling Statefull Widget"),),
-        body: Container(
-          padding: EdgeInsets.all(20.0),
-          child: Column(
-            children: [
-              TextField(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Simple Interest Calculator'),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(_minimumPadding * 2),
+        child: Column(
+          children: <Widget>[
+            getImageAsset(),
+            Padding(
+              padding: EdgeInsets.only(
+                  top: _minimumPadding, bottom: _minimumPadding),
+              child: TextField(
+                keyboardType: TextInputType.number,
                 decoration: InputDecoration(
-                  labelText: "Enter Student Intro",
-                  hintText: "Enter text",
+                  labelText: 'Principal',
+                  hintText: 'Enter Principal e.g. 12000',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
                 ),
-                onSubmitted: (String intro){
-                  // The most improtant function in all statefull widgets
-                  setState((){
-                    debugPrint("calling setState function");
-                    studentIntro=intro;
-                  });
-                },
               ),
-
-              Padding(
-                padding: const EdgeInsets.only(top: 30.0),
-                child: Text("Student Intro: $studentIntro",
-                style: TextStyle(fontSize: 25.0),),
-              )
-            ],
-          ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                  top: _minimumPadding, bottom: _minimumPadding),
+              child: TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Rate of Interest',
+                  hintText: 'In percent',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                  top: _minimumPadding, bottom: _minimumPadding),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        labelText: 'Term',
+                        hintText: 'Time in years',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: _minimumPadding * 5,
+                  ),
+                  Expanded(
+                    child: DropdownButton<String>(
+                      value: dropdownValue,
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          dropdownValue = newValue!;
+                        });
+                      },
+                      items: <String>['Rupee', 'Dollar', 'Pound', 'Euro']
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                  bottom: _minimumPadding, top: _minimumPadding),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: ElevatedButton(
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 15.0, bottom: 15.0),
+                        child: Text('Calculate'),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                  Container(
+                    width: _minimumPadding * 5,
+                  ),
+                  Expanded(
+                    child: ElevatedButton(
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 15.0, bottom: 15.0),
+                        child: Text('Reset'),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
+    );
+  }
+
+  Widget getImageAsset() {
+    AssetImage assetImage = AssetImage('images/money.png');
+    Image image = Image(
+      image: assetImage,
+      width: 125.0,
+      height: 125.0,
+    );
+
+    return Container(
+      child: image,
+      margin: EdgeInsets.all(_minimumPadding * 10),
     );
   }
 }
