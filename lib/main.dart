d7ed8459ef0212fcadd62f2916cbcf0d933ad377
Muhammad Afzal <imafzalakram@gmail.com>
2022-05-30 11:19:31 +0500

    ListView.Builder

--- lib/main.dart
+++ lib/main.dart
@@ -14,133 +14,24 @@ void main() {
 }
 
 class ListViewDesign extends StatelessWidget {
-  const ListViewDesign({Key? key}) : super(key: key);
-
+  List<String> months = ["April", "May", "June", "July", 'Aug','Sep','Oct','Nov','Dec','Jan', 'Feb','March', 'April'];
+  List<int> colorsList = [300,200,100,500,900,600,400,700,600,600,800,200,600];
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        Container(
+    return ListView.builder(
+      itemCount: months.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Container(
           height: 70,
-          color: Colors.amber.shade300,
+          color: Colors.amber[colorsList[index]],
           child: Center(
             child: Text(
-              "April",
+              "${months[index]}",
               style: TextStyle(fontSize: 20),
             ),
           ),
-        ),
-        Container(
-          height: 70,
-          color: Colors.amber.shade400,
-          child: Center(
-            child: Text(
-              "April",
-              style: TextStyle(fontSize: 20),
-            ),
-          ),
-        ),
-        Container(
-          height: 70,
-          color: Colors.amber.shade600,
-          child: Center(
-            child: Text(
-              "April",
-              style: TextStyle(fontSize: 20),
-            ),
-          ),
-        ),
-        Container(
-          height: 70,
-          color: Colors.amber.shade500,
-          child: Center(
-            child: Text(
-              "April",
-              style: TextStyle(fontSize: 20),
-            ),
-          ),
-        ),
-        Container(
-          height: 70,
-          color: Colors.amber.shade900,
-          child: Center(
-            child: Text(
-              "April",
-              style: TextStyle(fontSize: 20),
-            ),
-          ),
-        ),
-        Container(
-          height: 70,
-          color: Colors.amber.shade400,
-          child: Center(
-            child: Text(
-              "April",
-              style: TextStyle(fontSize: 20),
-            ),
-          ),
-        ),
-        Container(
-          height: 70,
-          color: Colors.amber.shade800,
-          child: Center(
-            child: Text(
-              "April",
-              style: TextStyle(fontSize: 20),
-            ),
-          ),
-        ),
-        Container(
-          height: 70,
-          color: Colors.amber.shade600,
-          child: Center(
-            child: Text(
-              "April",
-              style: TextStyle(fontSize: 20),
-            ),
-          ),
-        ),
-        Container(
-          height: 70,
-          color: Colors.amber.shade800,
-          child: Center(
-            child: Text(
-              "April",
-              style: TextStyle(fontSize: 20),
-            ),
-          ),
-        ),
-        Container(
-          height: 50,
-          color: Colors.amber.shade300,
-          child: Center(
-            child: Text(
-              "April",
-              style: TextStyle(fontSize: 20),
-            ),
-          ),
-        ),
-        Container(
-          height: 70,
-          color: Colors.amber.shade800,
-          child: Center(
-            child: Text(
-              "April",
-              style: TextStyle(fontSize: 20),
-            ),
-          ),
-        ),
-        Container(
-          height: 70,
-          color: Colors.amber.shade300,
-          child: Center(
-            child: Text(
-              "April",
-              style: TextStyle(fontSize: 20),
-            ),
-          ),
-        )
-      ],
+        );
+      },
     );
   }
 }
