3eee4f1966d1f32247986c8531688b78e90753f5
Muhammad Afzal <imafzalakram@gmail.com>
2022-06-06 14:55:37 +0500

    Complete webview example with BottomNavigationBar widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,107 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:hello_world/second.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: NavDrawer(),
-  ));
-}
+void main() => runApp(const MyApp());
 
-class NavDrawer extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyStatefulWidget(),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _selectedIndex = 0;
+  late WebViewController controller;
+
+  Set<String> urls = {
+    "https://burst.shopify.com/",
+    "https://burst.shopify.com/photos",
+    "https://burst.shopify.com/business-ideas/"
+  };
+
+  @override
+  void initState() {
+    super.initState();
+    // Enable virtual display.
+    if (Platform.isAndroid) WebView.platform = AndroidWebView();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    debugPrint(
+        "Tab is $_selectedIndex and element is ${urls.elementAt(_selectedIndex)}");
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Navigation Drawer"),
-
-      ),
-      drawer: Drawer(
-        child: ListView(
-          children: [
-            DrawerHeader(
-              child: Text("Drawer Header",
-                style: TextStyle(
-                    fontSize: 25,
-                    color: Colors.white
-                ),),
-              decoration: BoxDecoration(
-                color: Colors.lightBlue,
-              ),
-            ),
-            ListTile(
-              title: Text("Contact Us"),
-              onTap: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context){
-                  return SecondUI();
-                }));
-              },
-            ),
-            ListTile(
-              title: Text("Privacy Policy"),
-              onTap: (){
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: Text("Return Poliy"),
-              onTap: (){
-                Navigator.pop(context);
-
-              },
-            ),
-          ],
+      body: SafeArea(
+        child: Center(
+          child: WebView(
+            initialUrl: urls.elementAt(_selectedIndex),
+            javascriptMode: JavascriptMode.unrestricted,
+            onWebViewCreated: (c) {
+              controller = c;
+            },
+          ),
         ),
       ),
-      body: Center(child: Text("My App")),
+
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              color: Color(0xff141414),
+            ),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.photo_album,
+              color: Color(0xff141414),
+            ),
+            label: 'All Photos',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.business,
+              color: Color(0xff141414),
+            ),
+            label: 'Business Ideas',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.white,
+        onTap: (tabIndex) {
+          setState(() {
+            controller.loadUrl(urls.elementAt(_selectedIndex));
+            _selectedIndex = tabIndex;
+          });
+        },
+      ),
+    );
+  }
+
+  WebView buildWebview(int tabId) {
+    debugPrint("Tab is $tabId and element is ${urls.elementAt(tabId)}");
+    String url = urls.elementAt(tabId);
+    return WebView(
+      initialUrl: urls.elementAt(_selectedIndex),
+      onWebViewCreated: (c) {
+        controller = c;
+      },
     );
   }
 }
