9563b17c48dd7b3c09080e082d09404ff2462e70
Muhammad Afzal <imafzalakram@gmail.com>
2022-06-08 11:43:06 +0500

    Complete code of Http request

--- lib/main.dart
+++ lib/main.dart
@@ -1,107 +1,23 @@
-import 'dart:io';
-
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:http/http.dart' as http;
 
-void main() => runApp(const MyApp());
+Future<void> main() async {
+  var url = Uri.parse('https://yashigarments.com/mobile_api.php');
+  var response = await http.get(url);
+  Map<String, dynamic> res = jsonDecode(response.body);
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: MyStatefulWidget(),
-    );
-  }
-}
-
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
-
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  int _selectedIndex = 0;
-  late WebViewController controller;
-
-  Set<String> urls = {
-    "https://burst.shopify.com/",
-    "https://burst.shopify.com/photos",
-    "https://burst.shopify.com/business-ideas/"
-  };
-
-  @override
-  void initState() {
-    super.initState();
-    // Enable virtual display.
-    if (Platform.isAndroid) WebView.platform = AndroidWebView();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    debugPrint(
-        "Tab is $_selectedIndex and element is ${urls.elementAt(_selectedIndex)}");
-    return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: WebView(
-            initialUrl: urls.elementAt(_selectedIndex),
-            javascriptMode: JavascriptMode.unrestricted,
-            onWebViewCreated: (c) {
-              controller = c;
-            },
-          ),
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Http Request Example"),
+        ),
+        body: ListTile(
+          title: Text(res['name'],style: TextStyle(color: Colors.black,fontSize: 20),),
+          subtitle: Text("Roll no is ${res['roll_no'].toString()}"),
         ),
       ),
-
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.white,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.home,
-              color: Color(0xff141414),
-            ),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.photo_album,
-              color: Color(0xff141414),
-            ),
-            label: 'All Photos',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.business,
-              color: Color(0xff141414),
-            ),
-            label: 'Business Ideas',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.white,
-        onTap: (tabIndex) {
-          setState(() {
-            controller.loadUrl(urls.elementAt(_selectedIndex));
-            _selectedIndex = tabIndex;
-          });
-        },
-      ),
-    );
-  }
-
-  WebView buildWebview(int tabId) {
-    debugPrint("Tab is $tabId and element is ${urls.elementAt(tabId)}");
-    String url = urls.elementAt(tabId);
-    return WebView(
-      initialUrl: urls.elementAt(_selectedIndex),
-      onWebViewCreated: (c) {
-        controller = c;
-      },
-    );
-  }
+    ),
+  );
 }
