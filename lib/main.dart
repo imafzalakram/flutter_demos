d37eddccce43a31626ba0c1a3d1978bbdbc650f8
Muhammad Afzal <imafzalakram@gmail.com>
2022-06-13 11:30:29 +0500

    Saving premitive data into Shared Preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,92 +1,91 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:hello_world/PostDetails.dart';
-import 'package:http/http.dart' as http;
-import 'Post.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-
-Future<List<Post>> fetchPosts() async {
-  var url = Uri.parse('https://jsonplaceholder.typicode.com/posts');
-  var response = await http.get(url);
-  if(response.statusCode==200) {
-    List jsonResponse = jsonDecode(response.body);
-    return jsonResponse.map((data) => Post.fromJson(data)).toList();
-  } else{
-    throw Exception("Unexpected Result");
-  }
+void main() {
+  runApp(SharedPref());
 }
 
-
-void main()  {
-  runApp(DisplayPosts());
-}
-
-class DisplayPosts extends StatefulWidget {
-
-  @override
-  State<DisplayPosts> createState() => _DisplayPostsState();
-}
-
-class _DisplayPostsState extends State<DisplayPosts> {
-  late Future<List<Post>> postData;
-
-  @override
-  void initState(){
-    super.initState();
-    postData=fetchPosts();
-    debugPrint("Post data is ${postData.toString()}");
-  }
-
+class SharedPref extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Post post;
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Fetch JSON Array"),),
-        body: Center(
-          child: FutureBuilder<List<Post>>(
-            future: postData,
-            builder: (context, snapshot) {
-              if(snapshot.hasData) {
-                List<Post>? posts =snapshot.data;
-                return ListView.builder(
-                  itemCount: posts?.length ,
-                  itemBuilder: (context, index) {
-                    return Container(
-                      margin: EdgeInsets.all(5),
-                      child: Card(
-                        child: Container(
-                          padding: EdgeInsets.all(8),
-                          child: Column(
-                            children: [
-                              ListTile(
-                                title: Text(posts![index].body),
-                                subtitle: Text(posts![index].title),
-                                leading:  CircleAvatar(
-                                  backgroundImage: NetworkImage("https://picsum.photos/200/"),
-                                ),
-                                onTap: () {
-                                  Navigator.push(context, MaterialPageRoute(builder: (builder){
-                                    return PostDetails(posts[index]);
-                                  }));
-                                },
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    );
-                  },
-                );
-
-              } else if(snapshot.hasError){
-                return Text("${snapshot.error}");
-              }
-              return CircularProgressIndicator();
-            },
-          ),
+        appBar: AppBar(
+          title: Text("Shared Pref"),
+        ),
+        body: Column(
+          children: [
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  saveString();
+                  saveInt();
+                  saveDouble();
+                  saveBoolean();
+                },
+                child: Text("Save Data"),
+              ),
+            ),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  getStringData();
+                  getIntData();
+                  getDoubleData();
+                  getBooleanData();
+                },
+                child: Text("Print Data"),
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
+
+  saveString() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    sharedPreferences.setString("user_name", "Muhammad Ali");
+  }
+
+  saveInt() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    sharedPreferences.setInt("user_roll_no", 45);
+  }
+
+  saveDouble() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    sharedPreferences.setDouble("user_gpa", 3.38);
+  }
+
+  saveBoolean() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    sharedPreferences.setBool("is_muslim", true);
+  }
+
+
+  getStringData() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    String? userName = sharedPreferences.getString("user_name");
+    debugPrint("Username: $userName");
+  }
+
+  getIntData() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    int? userRollNo = sharedPreferences.getInt("user_roll_no");
+    debugPrint("Username: $userRollNo");
+  }
+
+  getDoubleData() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    double? gpa = sharedPreferences.getDouble("user_gpa");
+    debugPrint("Username: $gpa");
+  }
+
+  getBooleanData() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+   bool? isMuslim = sharedPreferences.getBool("is_muslim");
+    debugPrint("Username: $isMuslim");
+  }
+
 }
